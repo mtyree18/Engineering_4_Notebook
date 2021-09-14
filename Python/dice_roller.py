949adf743141e03b7580aa7982f0958eadecc7a7
Max <60942957+mtyree18@users.noreply.github.com>
2021-09-14 11:36:06 -0400

    Create dice_roller.py

---
+++ Python/dice_roller.py
@@ -0,0 +1,11 @@
+# Automatic Dice Roller
+
+# Written by Max Tyree
+
+from random import randint
+
+print ("Automatic Dice Roller")
+
+while True:
+    x = input("Press Enter to Roll")
+    print(randint(0,6))
