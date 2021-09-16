9266c0fd00e609aa3171e766b27ff960547d1fc6
Max <60942957+mtyree18@users.noreply.github.com>
2021-09-16 11:33:22 -0400

    Update spicy_calculator.py

--- Python/spicy_calculator.py
+++ Python/spicy_calculator.py
@@ -6,8 +6,8 @@
 
 a = int(input("Enter a number"))
 b = int(input("Enter a second number"))
-def doMath(operand, a, b):
-    if operand == "Sum":
+def doMath(operand, a, b): #establishing the function
+    if operand == "Sum": #giving criteria for the function and returning the value based on that criteria
         return a + b
     if operand == "Div":
         return a / b
@@ -17,6 +17,9 @@ def doMath(operand, a, b):
         return a / b
     if operand == "Mod":
         return a % b
+#made a for loop that goes from 1 to the value entered + 1 so that it goes 
+#all the way to the number. Then I multiplied 1 times the next number in the sequence
+#over and over, redefining the varialbe that was originally 1 to equal what it just got multiplied to
     if operand == "Fact1":
         fact = 1
         for i in range(1,a+1):
@@ -27,6 +30,7 @@ def doMath(operand, a, b):
         for i in range(1,b+1):
             fact = fact * i
         return fact
+#prints the doMath function as a string so that it can concatenate with the string that displays the current operand
 print("Sum: " + str(doMath("Sum", a, b)))
 print("Div: " + str(doMath("Div", a, b)))
 print("Prod: " + str(doMath("Prod", a, b)))
