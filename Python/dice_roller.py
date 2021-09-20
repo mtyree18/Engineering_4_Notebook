0ebccdaf8c15e1160db3e9a6aba22b32c5d57eec
Max <60942957+mtyree18@users.noreply.github.com>
2021-09-20 12:20:59 -0400

    Update dice_roller.py

--- Python/dice_roller.py
+++ Python/dice_roller.py
@@ -2,12 +2,12 @@
 
 # Written by Max Tyree
 
-from random import randint
+from random import randint #importing the tools I will need
 
 print ("Automatic Dice Roller")
 
-x = ""
+x = "" #establishing my variable outside the loop
 
-while x != "x":
-    x = input("Press Enter to Roll")
-    print(randint(0,6))
+while x != "x": #making it so that when you input x it stops the while loop
+    x = input("Press Enter to Roll") #gathering the input for when you want to roll the dice
+    print(randint(0,6)) #printing a random number 1-6
