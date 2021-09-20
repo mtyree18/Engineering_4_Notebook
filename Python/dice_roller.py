898d3743a6d1bb0e0d3309846f85874157cf7ac4
Max <60942957+mtyree18@users.noreply.github.com>
2021-09-20 12:18:54 -0400

    Update dice_roller.py

--- Python/dice_roller.py
+++ Python/dice_roller.py
@@ -6,8 +6,8 @@ from random import randint
 
 print ("Automatic Dice Roller")
 
-while True:
-    y = int(float(input("How many sides do you want on your dice?: ")))
-    z = int(float(input("How many dice do you want to roll?: ")))
-    x = int(float(input("Press Enter to Roll")))
-    print(randint(0,z*y*6))
+x = ""
+
+while x != "x":
+    x = input("Press Enter to Roll")
+    print(randint(0,6))
