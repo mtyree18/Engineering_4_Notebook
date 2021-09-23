65909bda228bdf44a833e63b348bb9375a5c3861
Max <60942957+mtyree18@users.noreply.github.com>
2021-09-23 12:07:33 -0400

    Update strings.py

--- Python/strings.py
+++ Python/strings.py
@@ -1,1 +1,9 @@
+# Python Program 03 - Strings and Loops (ENGR4)
 
+#Written by Max Tyree
+
+#9.23.2021
+
+L = list((input("Enter a sentence")).replace(" ", "-"))
+L.append("-")
+for x in L: print (x)
