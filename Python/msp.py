762757f53ba4cdce77f3efe1bb5b0d546fe471d6
Max <60942957+mtyree18@users.noreply.github.com>
2021-10-07 12:30:40 -0400

    Update msp.py

--- Python/msp.py
+++ Python/msp.py
@@ -10,10 +10,27 @@ def clearScreen():
 def wordLength():
     print (emptyString)
     
+def pinata():
+    if (len(missedGuess) >= 1):
+        print("_______")
+        print("      |")
+        print("      O")
+    if (len(missedGuess) == 2):
+        print("      |")
+    elif (len(missedGuess) == 3):
+        print("      |--  ")
+    elif (len(missedGuess) >= 4):
+        print("    --|--  ")
+    if (len(missedGuess) == 5):
+        print("      \ ")
+    elif (len(missedGuess) >= 6):
+        print("     /\ ")
+        print("YOU LOSE HAHAHA")
+        
+        
 word = input("Player 1 what's the word")
-guess = input("Player 2, guess a letter")
 length = len(word)
-emptyString = ("_ " * length)
+emptyString = ("_" * length)
 correctGuess = ""
 missedGuess = ""
 
@@ -21,14 +38,14 @@ wordLength()
 
 while (True):
     guess = input("Player 2, guess a letter")
-    for x in word:
-        if (guess == x):
-            correctGuess = correctGuess + guess
-        else:
-            missedGuess = missedGuess + guess
+    if (guess in word):
+        correctGuess = correctGuess + guess
+        print("Correct Guesses: " + correctGuess)
+    else:
+        missedGuess = missedGuess + guess
+        pinata()
+        
     for i in range (0, len(word)):
         if ((word[i]) in correctGuess):
-            emptyString = emptyString[:i] + guess + emptyString [i+1:]
-        else:
-            break
+            emptyString = emptyString[:i] + word[i] + emptyString [i+1:]
     print (emptyString)
