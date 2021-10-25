b0a2abbea024fcb7806f21c904342ba82c889f47
Max <60942957+mtyree18@users.noreply.github.com>
2021-10-25 12:49:19 -0400

    Update foobar1.py

--- Python/foobar1.py
+++ Python/foobar1.py
@@ -1,4 +1,9 @@
-data[]
-
 def solution(data, n):
-    
+    remove = []
+    for x in data:
+        num = data.count(x)
+        if num > n:
+            remove.append(x)
+    result = [x for x in data if x not in remove]
+    return result
+solution(data,n)
