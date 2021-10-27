b8e8024644d430912914774d1de11526f9dd43b2
Max <60942957+mtyree18@users.noreply.github.com>
2021-10-27 14:20:32 -0400

    Update code.py

--- Python/Foobar2/code.py
+++ Python/Foobar2/code.py
@@ -1,1 +1,13 @@
-
+def solution(pegs):
+    r0 = 0
+    coefficient = 0
+    distances = (len(pegs))-1
+    if (distances) % 2 == 0:
+        coefficient = 2/3
+    else:
+        coefficient = 2
+    for i in pegs:
+        print (i)
+        r0 = r0 + (coefficient( pegs[i + 1] - pegs[i] ))
+    
+solution([4, 30, 50])
