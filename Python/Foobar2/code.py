47762f8c06db70f94faba120e86add6d0d83f3b1
Max <60942957+mtyree18@users.noreply.github.com>
2021-10-27 18:30:48 -0400

    Update code.py

--- Python/Foobar2/code.py
+++ Python/Foobar2/code.py
@@ -2,16 +2,17 @@ def solution(pegs):
     r0 = 0
     coefficient = 0
     sign = -1
+    distance = []
     distances = (len(pegs))-1
     if (distances) % 2 == 0:
         coefficient = 2/3
     else:
         coefficient = 2
-    for i in pegs:
-        distance = pegs[i + 1] - pegs[i]
-        r0 = r0 - (sign * distance)
-        sign = -1 * sign
-        print (r0)
+    for i in range (distances):
+        distance = distance + [pegs[i + 1] - pegs[i]]
+    print (distance)
+    for x in range (distances):
+        #r1 = 2 (d1 - d2)
+        r0 = (distance[i] - distance[i+1])
     
-        
 solution([4, 30, 50])
