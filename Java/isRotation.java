c1ecc852fafe007b96a8d3b867b707f9729d2743
Max <60942957+mtyree18@users.noreply.github.com>
2021-11-05 15:40:53 -0400

    Create isRotation.java

---
+++ Java/isRotation.java
@@ -0,0 +1,32 @@
+public class isRotation{ 
+   static boolean isRotation(String var1, String var2) {
+       
+      //variables
+      String reverse = "";
+      boolean isReverse = false;
+      char ch;
+      
+      //reverses var1
+      for (int i=0; i < var1.length(); i++) {
+         ch = var1.charAt(i);
+         reverse = ch + reverse;
+      }
+      
+      //checks if the reversed var1 is equal to var2, and if it is it returns true, otherwise it returns false
+      System.out.println(var1);
+      System.out.println(reverse);
+      if (reverse.equals(var2) == true){
+         isReverse = true;
+      }
+      else if (reverse.equals(var2) == false){
+         isReverse = false;
+      }
+      System.out.println(isReverse);
+      return (isReverse);
+   }
+   
+   //calls the function
+   public static void main(String[] args) {
+      isRotation("racecar", "racecar");
+   }
+}
