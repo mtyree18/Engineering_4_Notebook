1aa42846e2e1420ad7da33c8f977592b69dce1e2
Max <60942957+mtyree18@users.noreply.github.com>
2021-11-08 09:54:17 -0500

    Update isRotation.java

--- Java/isRotation.java
+++ Java/isRotation.java
@@ -1,45 +1,31 @@
 public class isRotation{ 
-   static String isRotation(String var1, String var2) {
+   static boolean isRotation(String var1, String var2) {
        
       //variables
       String reverse = "";
       boolean isReverse = false;
-      char ch, var2Start;
-      int test;
+      char ch;
+      int test, pos;
       char firstLetter = var1.charAt(0);
-      var2Start = var2.indexOf(firstLetter);
+      pos = var2.indexOf(firstLetter);
       
-      System.out.print(var2Start);
-      return ("hi");
+      //loop checking for same letters
+      for (int i = 0; i < var1.length(); i++) {
+        if (pos == var1.length()) {
+            pos = 0;
+            if (var1.charAt(i) != var2.charAt(pos)) {
+                return false;
+            }
+            pos++;
+        }
+        System.out.print("i: " + i);
+        System.out.print("pos: " + pos);
+      }
+      return true;
    }
-        /*  
-      }
-      //reverses var1
-      for (int i=0; i < var1.length(); i++) {
-         ch = var1.charAt(i);
-         reverse = ch + reverse;
-      }
-      
-      //checks if the reversed var1 is equal to var2, and if it is it returns true, otherwise it returns false
-      System.out.println(var1);
-      System.out.println(reverse);
-      if (reverse.equals(var2) == true){
-         isReverse = true;
-      }
-      else if (reverse.equals(var2) == false){
-         isReverse = false;
-      }
-      System.out.println(isReverse);
-      return (isReverse);
-   }
-   */
 
    public static void main(String[] args) {
-      isRotation("racecar", "racecar");
+      isRotation("goat", "atgo");
    }
 
 }
-      isRotation("racecar", "racecar");
-   }
-}
-*/
