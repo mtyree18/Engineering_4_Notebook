4448cfbdb2df3cbde37bf7d6c1191e994b6bc169
Max <60942957+mtyree18@users.noreply.github.com>
2021-11-10 10:36:00 -0500

    Update isRotation.java

--- Java/isRotation.java
+++ Java/isRotation.java
@@ -2,13 +2,28 @@ public class isRotation{
     static boolean isRotation(String var1, String var2) {
         
         //variables
-        boolean endValue = true;
+        int var1Length;
         int pos;
-        char firstLetter = var1.charAt(0);
-        pos = var2.indexOf(firstLetter);
+        char firstLetter;
+        boolean endValue = true;
+        
+        if (var1.length() < 1 && var2.length() > 0) {
+            endValue = false;
+            var1Length = 0;
+        }
+        
+        else if (var1.length() < 1 && var2.length() < 1) {
+            endValue = true;
+            var1Length = 0;
+        }
+        else {
+            firstLetter = var1.charAt(0);
+            pos = var2.indexOf(firstLetter);
+            var1Length = var1.length();
+        }
         
         //loop checking for same letters
-        for (int i = 0; i < var1.length(); i++) {
+        for (int i = 0; i < var1Length; i++) {
             
             //says that if the first letter from var1 isn't found in var2, then break the loop
             if (pos < 0) {
@@ -17,26 +32,25 @@ public class isRotation{
             }
             
             // says that if the variables don't have the same length then return false
-            if (var1.length() != var2.length()) {
+            if (var1Length != var2.length()) {
                 endValue = false;
                 break;
             }
             
             //says to reset the position to 0 if it gets to the end of the word
-            if (pos == var1.length()) {
+            if (pos == var1Length) {
                 pos = 0;
             }
             
+            System.out.println("letter at pos " + pos + ": " + var2.charAt(pos));
+            System.out.println("letter at i " + i + ": " + var1.charAt(i));
+            
             /*because the value of i (what we are running the loop based off of)
             and pos should be the same, it makes sure of this by checking if they are the same.
-            If they are not the same then endValue is false because the words are not the same. This loop
-            also shows the value of i and pos so you can see any possible problems you might be having.
+            If they are not the same then endValue is false because the words are not the same
             */
             if (var1.charAt(i) != var2.charAt(pos)) {
                 endValue = false;
-                System.out.println("i: " + i);
-                System.out.println("letter at pos " + pos + ": " + var2.charAt(pos));
-                System.out.println("letter at i " + i + ": " + var1.charAt(i));
                 break;
             }
         pos++;
@@ -46,6 +60,6 @@ public class isRotation{
     }
 
     public static void main(String[] args) {
-        isRotation("y", "y");
+        isRotation("", "");
     }
 }
