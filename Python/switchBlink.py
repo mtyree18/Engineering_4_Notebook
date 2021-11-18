01da404db90c819735c791a43eb8d857c94f9394
Max <60942957+mtyree18@users.noreply.github.com>
2021-11-18 12:48:04 -0500

    Update switchBlink.py

--- Python/switchBlink.py
+++ Python/switchBlink.py
@@ -1,13 +1,13 @@
-from gpiozero import LED
-import RPi.GPIO as GPIO
-from time import sleep
-GPIO.setmode(GPIO.BCM)
+from gpiozero import LED #this library makes it easier to assign LEDs to pins.
+import RPi.GPIO as GPIO #imports the gpio library
+from time import sleep #makes it so that you just have to type sleep instead of time.sleep.
+GPIO.setmode(GPIO.BCM) #sets the GPIO mode to BCM
 
-green = LED(21)
+green = LED(21) #assigning leds to variables
 blue = LED(20)
 
 while(True):
-	blue.on()
+	blue.on() #blue goes on while green goes off for one second, then they switch
 	green.off()
 	sleep(1)
 	blue.off()
