9b4fa4424fd059a3a5068c5b3ea4ae389fa7e870
Max <60942957+mtyree18@users.noreply.github.com>
2021-12-09 11:28:00 -0500

    Update cdDefault.py

--- Python/cdDefault.py
+++ Python/cdDefault.py
@@ -1,1 +1,5 @@
 command = "cd /home/pi/Documents/Engineering_4_Notebook"
+import subprocess
+process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+output = process.communicate()[0]
+print(output)
