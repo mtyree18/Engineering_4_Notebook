ebe85d66f8b46e0033fd52ce4ded107b732b19ab
Max <60942957+mtyree18@users.noreply.github.com>
2021-12-14 12:40:36 -0500

    Update accelDisplay.py

--- Python/accelDisplay.py
+++ Python/accelDisplay.py
@@ -1,10 +1,11 @@
 import time
 import RPi.GPIO as GPIO
 
-import time
-
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_SSD1306
+import Adafruit_LSM303
+
+lsm303 = Adafruit_LSM303.LSM303()
 
 from PIL import Image
 from PIL import ImageDraw
@@ -32,20 +33,19 @@ width = disp.width
 height = disp.height
 image = Image.new('1', (width, height))
 
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
-
-# Draw a black filled box to clear the image.
-draw.rectangle((0,0,width,height), outline=0, fill=0)
-
 font = ImageFont.load_default()
 
 # Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
 # Some other nice fonts to try: http://www.dafont.com/bitmap.php
 #font = ImageFont.truetype('Minecraftia.ttf', 8)
 
+while True:
+  accel = lsm303.read()
+  accel_x, accel_y, accel_z = accel
+  time.sleep(0.5)
+
 # Write two lines of text.
-draw.text((x, top),    'Hello',  font=font, fill=255)
-draw.text((x, top+20), 'World!', font=font, fill=255)
+draw.text((x, top),    accel_x,  font=font, fill=255)
+draw.text((x, top+20), accel_y, font=font, fill=255)
 # Pin definition
 reset_pin = 24
