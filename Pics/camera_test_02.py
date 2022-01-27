11e88f0a27bbce07149f31ccfa277528a107dffc
mtyree18 <mtyree18@charlottesvilleschools.org>
2022-01-27 17:48:23 +0000

    added camera taker files

---
+++ Pics/camera_test_02.py
@@ -0,0 +1,14 @@
+import time
+import picamera
+
+effects = ['colorswap', 'blur', 'colorpoint', 'film', 'solarize']
+
+for i in effects:
+	with picamera.PiCamera() as camera:
+		print("running")
+		camera.resolution = (1024, 768)
+		camera.start_preview()
+		time.sleep(2)
+		camera.image_effect = i
+		camera.capture('pic.jpg')
+		print("done")
